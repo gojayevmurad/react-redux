0ac00d71ed83a17812e6da89b6c2e993829712a1
Murad Gojayev <muradqocayevi20@gmail.com>
2023-04-01 16:07:30 +0400

    redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+} from "./features/counterSlice";
+import { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch(increment());
+  };
+
+  const decrementHandler = () => {
+    dispatch(decrement());
+  };
+
+  const incrementByAmountHandler = () => {
+    dispatch(incrementByAmount(number));
+  };
+
+  const updateNumberHandler = (e) => {
+    if (isNaN(+e.target.value)) return;
+    setNumber(+e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter</h1>
+      <h2>{count}</h2>
+      <button onClick={() => incrementHandler()}>+</button>
+      <button onClick={() => decrementHandler()}>-</button>
+      <input type="text" onChange={(e) => updateNumberHandler(e)} />
+      <button onClick={() => incrementByAmountHandler()}>+{number}</button>
     </div>
   );
 }
